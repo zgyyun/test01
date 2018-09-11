a91b5f871260d43b0fa2f99dafc95797ed1a363e
zgyyun <zhou_164200@126.com>
2018-09-11 12:40:37 +0800

    gradle部署

---
+++ src/main/java/hello/hello.java
@@ -0,0 +1,15 @@
+package main.java.hello;
+
+public class hello {
+String color;
+String size;
+public static void main(String[] args)
+{
+	hello b=new hello();
+	b.color="黄色";
+	b.size="26尺寸";
+	
+	System.out.println("该自行车的颜色为："+b.color);
+	System.out.println("该自行车型号为："+b.size);
+	}
+}
